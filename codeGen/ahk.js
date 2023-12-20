a52403ba4bc005aa1beb8a863839792605c78df5
Banaanae <will.litch@outlook.com>
2023-12-20 16:31:02 +1100

    fix multiple blocks not generating code

--- codeGen/ahk.js
+++ codeGen/ahk.js
@@ -24,10 +24,19 @@ ahkGen.forBlock['math_number'] = function(block) {
 
 ahkGen.forBlock['sleep'] = function(block, generator) {
 	const delay = block.getFieldValue('sleep_delay');
-	return `sleep(${delay})\n`;
+	return `sleep(${delay})`;
 };
 
 ahkGen.forBlock['text'] = function(block) {
 	const textValue = block.getFieldValue('TEXT');
 	return [`"${textValue}"`, Order.ATOMIC];
 };
+
+ahkGen.scrub_ = function(block, code, thisOnly) {
+	const nextBlock =
+		block.nextConnection && block.nextConnection.targetBlock();
+	if (nextBlock && !thisOnly) {
+	  return code + '\n' + ahkGen.blockToCode(nextBlock);
+	}
+	return code;
+  };
