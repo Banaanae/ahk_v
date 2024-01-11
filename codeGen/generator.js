3748fc672884db917aec971b516caef3a1133be2
Banaanae <will.litch@outlook.com>
2024-01-11 12:11:52 +1100

    remove test code

--- codeGen/generator.js
+++ codeGen/generator.js
@@ -4,7 +4,5 @@ import * as blocks from './blockDef.js';
 export * from './ahkGen.js';
 
 export const ahkGen = new ahkgen();
-console.log(ahkGen)
-console.log(ahkGen.forBlock)
-console.log(blocks)
+
 Object.assign(ahkGen.forBlock,blocks);
